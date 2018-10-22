a2d19bfc88cced3837b72cf5f964bfa14a7be0e2
Clint Jang <jang.wangsu@gmail.com>
2018-10-22 11:35:34 +0900

    Add : Add Helloworld-swift.swift (인사 파일 추가)

---
+++ helloworld-swift.swift
@@ -0,0 +1,12 @@
+
+import Foundation
+
+struct AwesomeSwiftKoreanLecture {
+    static func hello() -> Void {
+        print("Awesome-Swift-Korean-Lecture에 방문해 주셔서 환영합니다. ")
+    }
+    
+    static func info() -> Void {
+        print("Swift 개발언어를 공부하기 위한 훌륭한 한국어 강의 동영상 정보 링크 모음 입니다.")
+    }
+}
